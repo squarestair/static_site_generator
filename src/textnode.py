29b09720094756e3f16485efcbca5bd2257775f6
squarestair <tim._wong@live.ca>
2026-03-02 22:56:41 -0500

    ch2_l01 completed

---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "plain"
+    BOLD_TEXT = "**Bold text**"
+    ITALIC_TEXT = "_Italic text_"
+    CODE_TEXT = "`Code text`"
+    LINKS = "[anchor text](url)"
+    IMAGES = "![alt text](url)"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node) -> bool:
+        te = text_node.text == self.text
+        tt = text_node.text_type == self.text_type
+        tu = text_node.url == self.url
+        return te and tt and tu
+
+    def __repr__(self) -> str:
+        return f'TextNode({self.text},{self.text_type},{self.url})'
