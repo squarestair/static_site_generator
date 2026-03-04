7e10a1aeb0daf3c29e6eb29be8f30f8da81eae18
squarestair <tim._wong@live.ca>
2026-03-03 22:31:16 -0500

    ch2_l04 completed

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -7,10 +7,10 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplementedError()
+        raise NotImplementedError("NotImplementedError")
 
     def props_to_html(self):
-        if self.props == None or '':
+        if not self.props:
             return ''
         res = ""
         for prop in self.props:
@@ -21,3 +21,17 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self,tag,value,props=None) -> None:
+        super().__init__(tag,value,None,props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("ValueError")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
