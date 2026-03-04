61122e8017a0a1fc769de1070adc21bc7c79cbb9
squarestair <tim._wong@live.ca>
2026-03-04 18:45:41 -0500

    ch2_l06 completed

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,5 +23,28 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node 1", TextType.BOLD,"text.com")
         node2 = TextNode("This is a text node 1", TextType.BOLD,"text.com")
         self.assertEqual(node, node2)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,14 @@
 from enum import Enum
 
+from htmlnode import LeafNode
+
 class TextType(Enum):
     TEXT = "plain"
     BOLD = "**Bold text**"
     ITALIC = "_Italic text_"
     CODE = "`Code text`"
-    LINKS = "[anchor text](url)"
-    IMAGES = "![alt text](url)"
+    LINK = "[anchor text](url)"
+    IMAGE = "![alt text](url)"
 
 
 class TextNode:
@@ -23,3 +25,26 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f'TextNode({self.text},{self.text_type},{self.url})'
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text, None)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text, None)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text, None)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text, None)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {
+                "href" : text_node.url
+            })
+        case TextType.IMAGE:
+            return LeafNode("img", "", {
+                "src" : text_node.url,
+                "alt" : text_node.text,
+            })
+        case _:
+            raise ValueError(f"ValueError: Incorrect text node type {text_node.text_type}")
+
