d4f7d3db54955b60fb9efc958c9a27debdd1a17d
squarestair <tim._wong@live.ca>
2026-03-04 22:02:42 -0500

    ch3_l01 completed

--- src/textnode.py
+++ src/textnode.py
@@ -24,7 +24,7 @@ class TextNode:
         return te and tt and tu
 
     def __repr__(self) -> str:
-        return f'TextNode({self.text},{self.text_type},{self.url})'
+        return f'TextNode("{self.text}",{self.text_type},{self.url})'
 
 def text_node_to_html_node(text_node):
     match text_node.text_type:
@@ -48,3 +48,22 @@ def text_node_to_html_node(text_node):
         case _:
             raise ValueError(f"ValueError: Incorrect text node type {text_node.text_type}")
 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        sections = node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Unclosed tags")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i],TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i],text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
